dc866510035f85acd43fe7c60bc8f5918bd35086
fineartit <fineartit@outlook.com>
2024-08-18 14:26:45 +0800

    copy some files.

---
+++ docs/source/conf.py
@@ -0,0 +1,66 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+from datetime import date
+
+project = "Enterprise Architecture"
+copyright = f"{date.today().year}, 深圳市精艺信息技术有限公司"
+author = "Advisor"
+release = "1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx_design",
+    "sphinxcontrib.mermaid",
+]
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "attrs_block",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+source_suffix = {
+    ".md": "markdown",
+}
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+language = "zh_CN"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = "alabaster"
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+html_title = ""
+html_theme_options = {
+    "home_page_in_toc": True,
+    "use_sidenotes": True,
+}
+
+# -- Options for Mermaid output -------------------------------------------------
+# https://sphinxcontrib-mermaid-demo.readthedocs.io/en/latest/
+
+mermaid_version = "10.9.1"
+html_js_files = [
+   "js/mermaid.js",
+]
---
+++ docs/source/index.md
@@ -0,0 +1,25 @@
+# 企业架构学
+
+## 实践指南
+
+旨在帮助您快速了解并掌握企业架构的基本知识、核心内容和设计方法，并结合一系列最佳实践加深对架构的理解，强化解决实际架构问题的能力。
+
+```{toctree}
+:maxdepth: 2
+:numbered:
+:caption: 📚实践指南
+
+contents/practical_guides/1_introduction.md
+contents/practical_guides/2_approach.md
+```
+
+## 扩展阅读
+
+多角度探索企业架构的历史变迁和发展趋势，以史为鉴，因应现实，远观未来。
+
+```{toctree}
+:maxdepth: 1
+:caption: 📖 扩展阅读
+
+contents/extended_reading/ibm_bsp.md
+```
