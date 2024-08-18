d75fb828ee7b1901b0cdbca1f0d1c3058816c710
fineartit <fineartit@outlook.com>
2024-08-18 17:07:10 +0800

    add enterprise_architecture package

--- enterprise_architecture/__init__.py
+++ enterprise_architecture/__init__.py
@@ -1,1 +1,10 @@
+"""
+Enterprise Architecture - Best Practices in Enterprise Architecture.
+"""
+
 __version__ = "1.0"
+
+
+class InvalidKindError(Exception):
+    """Raised if the kind is invalid."""
+    pass
